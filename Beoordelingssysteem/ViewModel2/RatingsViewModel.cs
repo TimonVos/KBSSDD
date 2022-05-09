aea75746813c66d7a7b164ec8f57c52a605beb06
Cortie <xcortie@gmail.com>
2022-05-09 14:52:41 +0200

    projecten opnieuw aangemaakt in verband met onbrekende frameworks. formulier textboxen omgezet naar radiobuttons voor selectie.

---
+++ Beoordelingssysteem/ViewModel2/RatingsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    internal class RatingsViewModel
+    {
+        public ICommand RatingCommand { get; set; }
+        public RatingsViewModel()
+        {
+            //RatingCommand = new 
+        }
+ 
+    }
+}
