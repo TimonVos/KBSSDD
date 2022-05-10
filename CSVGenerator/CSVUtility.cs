ce03b15218428cd3bf37162fe4e14388c2c7842a
TimonVos <74247704+TimonVos@users.noreply.github.com>
2022-05-10 14:37:57 +0200

    initial commit

---
+++ CSVGenerator/CSVUtility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CSVGenerator
+{
+    internal static class CSVUtility
+    {
+        public static void ToCSV(DataTable dt, string filePath)
+        {
+            StreamWriter sw = new StreamWriter(filePath, false);
+            //headers    
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                sw.Write(dt.Columns[i]);
+                if (i < dt.Columns.Count - 1)
+                {
+                    sw.Write(",");
+                }
+            }
+            sw.Write(sw.NewLine);
+            foreach (DataRow dr in dt.Rows)
+            {
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    if (!Convert.IsDBNull(dr[i]))
+                    {
+                        string value = dr[i].ToString();
+                        if (value.Contains(','))
+                        {
+                            value = String.Format("\"{0}\"", value);
+                            sw.Write(value);
+                        }
+                        else
+                        {
+                            sw.Write(dr[i].ToString());
+                        }
+                    }
+                    if (i < dt.Columns.Count - 1)
+                    {
+                        sw.Write(",");
+                    }
+                }
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ CSVGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable table = new DataTable();
+            //columns  
+            table.Columns.Add("ID", typeof(int));
+            table.Columns.Add("NAME", typeof(string));
+            table.Columns.Add("CITY", typeof(string));
+
+            //data  
+            table.Rows.Add(111, "Devesh", "Ghaziabad");
+            table.Rows.Add(222, "ROLI", "KANPUR");
+            table.Rows.Add(102, "ROLI", "MAINPURI");
+            table.Rows.Add(212, "DEVESH", "KANPUR");
+            table.Rows.Add(102, "NIKHIL", "GZB");
+            table.Rows.Add(212, "HIMANSHU", "NOIDa");
+            table.Rows.Add(102, "AVINASH", "NOIDa");
+            table.Rows.Add(212, "BHUPPI", "GZB");
+
+            CSVUtility.ToCSV(table, "C:\\temp\\testcsv.csv");
+        }
+    }
+}
