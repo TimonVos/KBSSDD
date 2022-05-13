536d5d7c4d289de2b97a029536a11baa976d676f
Cortie <xcortie@gmail.com>
2022-05-13 14:56:34 +0200

    merge problemen met sprint 1 opgelost

---
+++ Model/Competence.cs
@@ -0,0 +1,18 @@
+﻿namespace Model
+{
+    public class Competence
+    {
+        #region Properties
+
+        public string? Name { get; set; }
+        public string[]? RatedBy { get; set; }
+        public List<Criterium>? Criteriums { get; set; }
+        public string? Notes { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        #endregion
+    }
+}
