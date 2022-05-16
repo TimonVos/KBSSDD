cad8497279e01906cabc0fbfcece25bafecbf774
Cortie <xcortie@gmail.com>
2022-05-16 10:52:00 +0200

    renamed formservices to assessmentservices

--- Services/AssessmentServices/AssessmentHelper.cs
+++ Services/AssessmentServices/AssessmentHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Service.FormServices
+namespace Service.AssessmentServices
 {
     internal class AssessmentHelper
     {
