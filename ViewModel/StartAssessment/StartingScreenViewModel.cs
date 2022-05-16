40ce151ba64ed3c8729f8b4a30e45343aa23c00a
dilano <swankytrunks@gmail.com>
2022-05-16 22:31:40 +0200

    Starting screen setup
    Missing databinding

---
+++ ViewModel/StartAssessment/StartingScreenViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace ViewModel.StartAssessment
+{
+    public class StartingScreenViewModel
+    {
+        SelectForm form1 = new SelectForm(1, "OOSDD2");
+        SelectForm form2 = new SelectForm(2, "Stage/afstuderen of Project");
+    }
+
+}
