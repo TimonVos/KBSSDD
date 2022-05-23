202ba9eb1adf7735ee3abdcfb31c11bb5ce1596b
Fox <74247704+TimonVos@users.noreply.github.com>
2022-05-23 12:48:27 +0200

    sync before school

--- View/GroupSelection/GroupSelection.xaml.cs
+++ View/GroupSelection/GroupSelection.xaml.cs
@@ -30,6 +30,10 @@ namespace View
 
         private void GroupSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
+            if (this.DataContext is GroupSelectionViewModel VM)
+            {
+                //VM.SelectedGroupChanged.invoke(this, this.GroupListBox.SelectedIndex);
+            }
             // this.DataContext.Sele
             // hoe ga ik het zo binden dat ik mijn event hier goed kan invoken? 
             // ik heb gegeken ernaar om het in de GroupSelection.g.i.cs aan te passen om zo het smooth te koppelen
--- ViewModel/GroupSelection/GroupSelectionViewModel.cs
+++ ViewModel/GroupSelection/GroupSelectionViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
 using System.Windows;
 using Model;
@@ -12,11 +13,11 @@ namespace ViewModel
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        public delegate int SelectedGroupChanged(object sender, int index);
-
         public RelayCommand AddGroup { get; set; }
         public RelayCommand RemoveGroup { get; set; }
         public ObservableCollection<Group> Groups { get; set; }
+
+
         private ObservableCollection<string> _groupNames = new ObservableCollection<string>();
 
         public ObservableCollection<string> GroupNames
@@ -33,23 +34,48 @@ namespace ViewModel
             set { _groupNames = value; }
         }
 
+
+        // TextBox Content
         public string GroupName { get; set; }
         public string SelectedGroup { get; set; }
 
+        private int _selectedGroupIndex;
+
+        public int SelectedGroupIndex
+        {
+            get { return _selectedGroupIndex; }
+            set {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedGroupIndex)));
+                _selectedGroupIndex = value; 
+            }
+        }
+
+
+        //STUDENT PROPERTIES____________
 
         public RelayCommand AddStudent { get; set; }
         public RelayCommand RemoveStudent { get; set; }
-        public string StudentName { get; set; }
-        public string StudentNumber { get; set; }
-        public string SelectedStudent { get; set; }
 
+
+        /// <summary>
+        /// Student Name TextBox Content
+        /// </summary>
+        public string StudentName { get; set; }
+        /// <summary>
+        /// Student Number TextBox Content
+        /// </summary>
+        public string StudentNumber { get; set; }
+
+        /// <summary>
+        /// List of studentnames
+        /// </summary>
         private ObservableCollection<string> _studentNames = new ObservableCollection<string>();
 
         public ObservableCollection<string> StudentNames
         {
             get
             {
-                
+                _studentNames.Clear();
                 foreach (var GroupInstance in Groups)
                 {
                     if (GroupInstance.Name.Equals(SelectedGroup))
@@ -68,6 +94,7 @@ namespace ViewModel
         public GroupSelectionViewModel()
         {
 
+
             //GROUPS________________________________________
             GroupName = "Groep";
 
@@ -75,7 +102,10 @@ namespace ViewModel
             Groups.Add(new Group("Groep 1"));
             Groups.Add(new Group("Groep 2"));
 
-            Groups[0].Students.Add(new Student(1158062, "Timon Vos"));
+            Groups[SelectedGroupIndex].Students.Add(new Student(1158062, "Timon Vos"));
+            Groups[0].Students.Add(new Student(1145823, "Jan Doedel"));
+
+            Groups[1].Students.Add(new Student(1130293, "Harm de Boer"));
 
             AddGroup = new RelayCommand(() =>
             {
@@ -84,13 +114,7 @@ namespace ViewModel
 
             RemoveGroup = new RelayCommand(() =>
             {
-                foreach (var GroupInstance in Groups)
-                {
-                    if (GroupInstance.Name.Equals(SelectedGroup))
-                    {
-                        Groups.Remove(GroupInstance);
-                    }
-                }
+                Groups.RemoveAt(SelectedGroupIndex);
             });
 
 
@@ -102,7 +126,7 @@ namespace ViewModel
 
             AddStudent = new RelayCommand(() =>
             {
-                
+                Groups[SelectedGroupIndex].Students.Add(new Student(int.Parse(StudentNumber), StudentName));
             });
 
             RemoveStudent = new RelayCommand(() =>
@@ -110,9 +134,11 @@ namespace ViewModel
                 
                 
             });
-            
+        }
 
-            
+        private void OnSelectionChanged(object sender, int index)
+        {
+            this.SelectedGroupIndex = index;
         }
     }
 }
